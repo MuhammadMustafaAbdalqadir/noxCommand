3a180d7930c704eee3f67728d5a8a233878d2620
Muhammad Mustafa <33350449+MuhammadMustafaAbdalqadir@users.noreply.github.com>
2017-12-08 16:11:20 +0200

    Add fancy printing & get home dir

--- noxCommand.cpp
+++ noxCommand.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <dirent.h>
+#include <windows.h>
 
 using namespace std;
 
+
+
+
+string current_path, user;
+
+//Fancy printing
+void fp(string str){
+    int n = str.length();
+    for (int i = 0 ; i < n; i ++){
+        cout << str[i]; Sleep(20);
+    }
+}
+
+void get_home_dir(){
+    current_path = getenv("USERPROFILE");
+}
+
 int main(){
 
+    system("color A");
+    get_home_dir();
 
 
 
